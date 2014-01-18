22f2e26874fe82a1b41bc5ed696eac152520470d
Jonas Gessner <contact@j-gessner.de>
2014-01-18 19:28:15 +0100

    Updated NC template

--- Notification_Center_Widget.nic/@@PROJECTNAME@@ViewController.m
+++ Notification_Center_Widget.nic/@@PROJECTNAME@@ViewController.m
@@ -90,9 +90,10 @@
 
 - (NSURL *)launchURLForTapLocation:(CGPoint)point {
     UITouch *touch = [[UITouch alloc] initWithPoint:[self.view convertPoint:point toView:self.view.window] andView:self.view];
+    
     UIEvent *eventDown = [[UIEvent alloc] initWithTouch:touch];
     [touch.view touchesBegan:[eventDown allTouches] withEvent:eventDown];
-    [touch changeToPhase:UITouchPhaseEnded];
+    [touch setPhase:UITouchPhaseEnded];
     
     UIEvent *eventUp = [[UIEvent alloc] initWithTouch:touch];
     [touch.view touchesEnded:[eventUp allTouches] withEvent:eventUp];
